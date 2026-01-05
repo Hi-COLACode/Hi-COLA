13324a2ede15a91e8b546605da4bcd388fcf6f4f
Krishna Naidoo <krishna.naidoo.11@ucl.ac.uk>
2026-01-05 18:30:20 +0100

    update to init file

--- HiCOLA/__init__.py
+++ HiCOLA/__init__.py
@@ -1,6 +1,5 @@
 
 from .Frontend.model import HorndeskiModel
-from .Frontend.modelv2 import HorndeskiModelV2
 
 from .Frontend.lcdm import compute_Ez_LCDM
 from .Frontend.lcdm import compute_Hz_LCDM
