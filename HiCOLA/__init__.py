5a8389adcf51dfe58de5b0142edb42f51478f9c6
Krishna Naidoo <krishna.naidoo.11@ucl.ac.uk>
2026-01-08 19:36:40 +0000

    split lcdm and horndeski parts of model class to make the code easier to follow

--- HiCOLA/__init__.py
+++ HiCOLA/__init__.py
@@ -1,12 +1,5 @@
 
-from .Frontend.model import HorndeskiModel
-
-from .Frontend.lcdm import compute_Ez_LCDM
-from .Frontend.lcdm import compute_Hz_LCDM
-from .Frontend.lcdm import compute_Omega_r_z_LCDM
-from .Frontend.lcdm import compute_Omega_m_z_LCDM
-from .Frontend.lcdm import compute_Omega_l_z_LCDM
-from .Frontend.lcdm import compute_EprimeE_x_LCDM
-from .Frontend.lcdm import compute_Omega_l_prime_LCDM
+from .Frontend.model_standard import StandardModel
+from .Frontend.model_horndeski import HorndeskiModel
 
 from .Frontend.redshift import z2a, a2z, a2x, x2a, z2x, x2z
